adc3aab5039480a54bb262c0d862244a216291a0
SamarpreetSingh787 <78362149+SamarpreetSingh787@users.noreply.github.com>
2021-04-12 15:39:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload() {
+   clown_nose = loadImage("https://i.postimg.cc/c1B2C3K1/red-nose.png");
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log("PoseNet Is Initialized");
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+  //circle(nose_x, nose_y, 20);
+  image(clown_nose, nose_x, nose_y, 30, 30);
+}
+
+function gotPoses(results) {
+  if (results.length > 0) {
+    console.log(results);
+    nose_x = results[0].pose.nose.x-15;
+    nose_y = results[0].pose.nose.y-15;
+    console.log("Nose x =" + nose_x);
+    console.log("Nose y =" + nose_y);
+  }
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
